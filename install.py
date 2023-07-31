5e214633ba74b4ba8b52b44d0fe4007bf3b6cb6c
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2023-07-31 11:49:42 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,11 +1,15 @@
 import os
 from utils.file import get_path, get_username
+import argparse
 
 if __name__ == '__main__':
     path = get_path()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--ros", type=str, default='noetic')
+    args = parser.parse_args()
     with open('start.sh', 'w') as f:
         f.write("#!/bin/bash\n")
-        f.write("source /opt/ros/noetic/setup.bash\n")
+        f.write(f"source /opt/ros/{args.ros}/setup.bash\n")
         f.write(f"source /home/{get_username()}/geoscan_ws/devel/setup.bash\n")
         f.write(f"python3 {path}/web-menu.py\n")
 
